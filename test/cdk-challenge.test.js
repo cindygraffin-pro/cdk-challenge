850c0114acb66598890458bea05e6a3a0925ec66
cindygraffin-pro <cindy.graffin-cic@ibm.com>
2022-10-30 12:01:35 +0100

    Initial commit

---
+++ test/cdk-challenge.test.js
@@ -0,0 +1,17 @@
+// const cdk = require('aws-cdk-lib');
+// const { Template } = require('aws-cdk-lib/assertions');
+// const CdkChallenge = require('../lib/cdk-challenge-stack');
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-challenge-stack.js
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//   // WHEN
+//   const stack = new CdkChallenge.CdkChallengeStack(app, 'MyTestStack');
+//   // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
